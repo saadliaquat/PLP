b7e41d4f88847c5319ab6311c60c1947fcca54de
Saad Liaquat <25100259@lums.edu.pk>
2024-08-17 18:44:00 +0500

    Initial website

---
+++ src/app/about/page.jsx
@@ -0,0 +1,28 @@
+
+
+const About= () => {
+    return(
+    <main>
+        <section class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
+            <div class="px-4 mx-auto max-w-screen-xl text-left lg:py-56 py-80 text-center">
+                <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">About us</h1>
+                <p class="mb-8 text-lg leading-loose font-normal text-gray-300 lg:text-xl sm:px-8 lg:px-16 text-justify">Purposeful Learning Partnership is dedicated to transforming education through innovative training and support for educators and families. Our organization was founded with the belief that every teacher, school leader deserves access to the best resources and professional development opportunities, regardless of their school's financial status.
+                    We provide tailored training programs for teachers and professional development for school heads. Our unique model offers free or low-cost training to low-income schools while providing premium services to well-performing schools, ensuring our programs are accessible to all. Additionally, as part of our welfare project, we offer free community sessions for parents on citizenship, character building, and education about parenting. These sessions are designed to empower families and create a supportive environment that nurtures the holistic development of childre
+                    At Purposeful Learning Partnership, we believe that empowering educators and engaging parents are key to enhancing futures. By fostering a culture of continuous learning and improvement, we aim to create a positive and lasting impact on the educational landscape, and on society in general by benefiting students, teachers, parents, and communities alike. Join us in our mission to empower education and enhance futures for generations to come.</p>
+            </div>
+        </section>
+
+        <section class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
+            <div class="px-4 mx-auto max-w-screen-xl text-left lg:py-56 py-80 text-center">
+                <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Our Team</h1>
+                <p class="mb-8 text-lg leading-loose font-normal text-gray-300 lg:text-xl sm:px-8 lg:px-16 text-justify">
+                    Director</p>
+                    <p class="mb-8 text-lg leading-loose font-normal text-gray-300 lg:text-xl sm:px-8 lg:px-16 text-justify">
+                    Deupty Director</p>
+            </div>
+        </section>
+    </main>
+    )
+}
+
+export default About;
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,69 @@
+const contactUs = () => {
+    return (
+        <section class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
+            <div className="flex flex-col items-center justify-around">
+            <div class="md:flex flex-row justify-around px-4 max-w-screen-xl text-left lg:pt-56 pt-80 pb-4 text-center">
+                <div class="flex-1 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-4 md:mb-0 md:mr-3">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Contact Number</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+                    <a href="#" class="inline-flex font-medium items-center text-blue-600 hover:underline">
+                        Contact us
+                        <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
+                        </svg>
+                    </a>
+                </div>
+
+                <div class="flex-1 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Email</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+                    <a href="#" class="inline-flex font-medium items-center text-blue-600 hover:underline">
+                        Email us
+                        <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
+                        </svg>
+                    </a>
+                </div>
+
+            </div>
+
+            <div class="md:flex flex-row justify-around px-4 max-w-screen-xl text-left pb-4 text-center">
+                <div class="flex-1 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 md:mb-0 mb-4 md:mr-3">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Facebook</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+                    <a href="#" class="inline-flex font-medium items-center text-blue-600 hover:underline">
+                        Visit our page
+                        <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
+                        </svg>
+                    </a>
+                </div>
+
+                <div class="flex-1 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 md:mb-0 mb-4">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Instagram</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+                    <a href="#" class="inline-flex font-medium items-center text-blue-600 hover:underline">
+                        Visit our page
+                        <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
+                        </svg>
+                    </a>
+                </div>
+
+            </div>
+            </div>
+
+            
+        </section>
+    )
+}
+
+export default contactUs;
